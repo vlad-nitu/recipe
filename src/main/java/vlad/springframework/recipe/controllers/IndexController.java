6e3f569d8f8b91775be6cc0d8967ee179c8421bb
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-16 21:46:27 +0300

    Created Index Page

---
+++ src/main/java/vlad/springframework/recipe/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package vlad.springframework.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
