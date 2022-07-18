1ac15ea865a013ae9db679641fb1c5d5e8ea8a4d
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-18 14:11:32 +0300

    Added Enum Recipe & 1-1 relationship
    Took 6 minutes

---
+++ src/main/java/vlad/springframework/recipe/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package vlad.springframework.recipe.domain;
+
+public enum Difficulty {
+    EASY,
+    MODERATE,
+    HARD
+}
--- src/main/java/vlad/springframework/recipe/domain/Recipe.java
+++ src/main/java/vlad/springframework/recipe/domain/Recipe.java
@@ -16,8 +16,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
@@ -104,5 +104,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
