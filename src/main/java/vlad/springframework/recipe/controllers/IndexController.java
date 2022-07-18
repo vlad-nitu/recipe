e7034882e149e347f6022fb2250fac417f65a782
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-18 13:42:06 +0300

    Created 1-1 realtionship between Notes and Recipe classes
    Took 28 minutes

--- src/main/java/vlad/springframework/recipe/controllers/IndexController.java
+++ src/main/java/vlad/springframework/recipe/controllers/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        System.out.println("Enabled Automatic Restart for Spring");
         return "index";
     }
 }
