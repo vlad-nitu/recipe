ff4cadab0a379820ca457b7d4e8af25f4dd4f220
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-18 14:05:21 +0300

    Added 1-1 Ingredient - Unit Of Measure relatioship
    Took 9 minutes

--- src/main/java/vlad/springframework/recipe/domain/Ingredient.java
+++ src/main/java/vlad/springframework/recipe/domain/Ingredient.java
@@ -10,10 +10,18 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    //private UnitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getDescription() {
         return description;
     }
@@ -36,6 +44,14 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
 
