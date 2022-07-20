c6a2dbb39a2ccd734b393eace9877232683acf5f
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-20 19:40:46 +0300

    Refactored test method to use ArgumentCaptor
    Took 11 minutes

--- src/test/java/vlad/springframework/recipe/controllers/IndexControllerTest.java
+++ src/test/java/vlad/springframework/recipe/controllers/IndexControllerTest.java
@@ -3,6 +3,7 @@ package vlad.springframework.recipe.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import vlad.springframework.recipe.services.RecipeService;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,11 +33,19 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
-        Recipe recipe = new Recipe();
-        Set<Recipe> recipesData = Set.of(recipe);
+        Recipe recipe1 = new Recipe();
+        Recipe recipe2 = new Recipe();
+        recipe1.setDescription("description 1");
+        recipe1.setDescription("description 2");
+
+        Set<Recipe> recipesData = Set.of(recipe1, recipe2);
+        ArgumentCaptor<Set<Recipe>> captor = ArgumentCaptor.forClass(Set.class);
         when(recipeService.getRecipes()).thenReturn(recipesData);
+
         String viewPage = indexController.getIndexPage(model); //Call method under test
+
         assertThat(viewPage).isEqualTo("index");
-        verify(model).addAttribute("recipes", recipesData);
+        verify(model).addAttribute(eq("recipes"), captor.capture());
+        assertThat(recipesData).isEqualTo(captor.getValue());
     }
 }
