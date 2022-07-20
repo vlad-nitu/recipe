5c18fb223b28ac600707607f6c7288b61f42744d
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-20 20:21:41 +0300

    Added maven-failsafe-plugin to run integration tests separately
    Took 15 minutes

--- src/test/java/vlad/springframework/recipe/repositories/UnitOfMeasureRepositoryTestIT.java
+++ src/test/java/vlad/springframework/recipe/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -2,10 +2,8 @@ package vlad.springframework.recipe.repositories;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import vlad.springframework.recipe.domain.UnitOfMeasure;
 
 import java.util.Optional;
@@ -13,7 +11,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-class UnitOfMeasureRepositoryTest {
+class UnitOfMeasureRepositoryTestIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
