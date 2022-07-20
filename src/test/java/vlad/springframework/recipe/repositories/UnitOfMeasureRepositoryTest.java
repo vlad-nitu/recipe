18fb6b2dc692e9831a422976c408423b1c741b5d
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-20 20:06:46 +0300

    Created Integration test for UOMRepository
    Took 13 minutes

---
+++ src/test/java/vlad/springframework/recipe/repositories/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,35 @@
+package vlad.springframework.recipe.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import vlad.springframework.recipe.domain.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescriptionTeaspoon() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
