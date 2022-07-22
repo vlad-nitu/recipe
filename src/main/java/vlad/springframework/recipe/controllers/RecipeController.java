6295399d639a815eb1cfadb41c7a194a689954ea
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-22 17:42:12 +0300

    Add a Recipe functionality
    Took 39 minutes

--- src/main/java/vlad/springframework/recipe/controllers/RecipeController.java
+++ src/main/java/vlad/springframework/recipe/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package vlad.springframework.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vlad.springframework.recipe.commands.RecipeCommand;
 import vlad.springframework.recipe.services.RecipeService;
 
 @Controller
@@ -19,7 +22,16 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
         return "recipe/show";
-
-
+    }
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
