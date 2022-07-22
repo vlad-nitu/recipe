685c16249b2021e181dcfe37adf91ca2c8feccd9
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-22 19:20:32 +0300

    Created new view page to log ingredients
    Took 22 minutes

---
+++ src/test/java/vlad/springframework/recipe/controllers/IngredientControllerTest.java
@@ -0,0 +1,45 @@
+package vlad.springframework.recipe.controllers;
+
+import lombok.experimental.StandardException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.creation.bytebuddy.MockAccess;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import vlad.springframework.recipe.commands.RecipeCommand;
+import vlad.springframework.recipe.services.RecipeService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class IngredientControllerTest {
+    @Mock
+    private RecipeService recipeService;
+    private IngredientController controller;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+    @Test
+    void listIngredients() throws Exception{
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+        verify(recipeService).findCommandById(anyLong());
+    }
+}
