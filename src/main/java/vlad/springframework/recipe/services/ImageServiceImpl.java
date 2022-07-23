1bf1296549b3803d783fd4fbe6eca1add5a1d95e
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-23 18:22:06 +0300

    Persist added image to Database
    Check the logs to see if a message regarding how many bytes the saved image has is displayed. Also, you can access the h2-console to see that an instance of image is persisted to the specified Recipe instance
    
    Took 15 minutes

--- src/main/java/vlad/springframework/recipe/services/ImageServiceImpl.java
+++ src/main/java/vlad/springframework/recipe/services/ImageServiceImpl.java
@@ -4,12 +4,37 @@ package vlad.springframework.recipe.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import vlad.springframework.recipe.domain.Recipe;
+import vlad.springframework.recipe.repositories.RecipeRepository;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
-       log.debug("File received");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int k = 0;
+            for (byte b : file.getBytes()){
+                byteObjects[k++] = b;
+            }
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+            log.info("An image of " + byteObjects.length + " bytes was successfully saved.");
+
+        } catch (IOException e) {
+            //TODO: handle exception
+            log.error("Error occurred", e);
+        }
     }
 }
---
+++ src/test/java/vlad/springframework/recipe/services/ImageServiceImplTest.java
@@ -0,0 +1,56 @@
+package guru.springframework.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import vlad.springframework.recipe.domain.Recipe;
+import vlad.springframework.recipe.repositories.RecipeRepository;
+import vlad.springframework.recipe.services.ImageService;
+import vlad.springframework.recipe.services.ImageServiceImpl;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class ImageServiceImplTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    ImageService imageService;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void saveImageFile() throws Exception {
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+                "Spring Vlad".getBytes());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        imageService.saveImageFile(id, multipartFile);
+
+        verify(recipeRepository).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+    }
+
+}
