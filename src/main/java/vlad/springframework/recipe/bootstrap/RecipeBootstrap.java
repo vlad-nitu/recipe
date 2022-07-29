efec23212b234292cb05571f7f83f069d2d5fd7e
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-30 01:57:31 +0300

    Refactor DB Initalisation for MySQL
    Took 1 hour 16 minutes

--- src/main/java/vlad/springframework/recipe/bootstrap/RecipeBootstrap.java
+++ src/main/java/vlad/springframework/recipe/bootstrap/RecipeBootstrap.java
@@ -2,6 +2,7 @@ package vlad.springframework.recipe.bootstrap;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import vlad.springframework.recipe.domain.*;
@@ -17,6 +18,7 @@ import java.util.Optional;
 
 @Slf4j
 @Component
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private final CategoryRepository categoryRepository;
     private final RecipeRepository recipeRepository;
